28c047088174ef068e7333c923d9ef13d0645f92
hamzaisrar16 <153775137+hamzaisrar16@users.noreply.github.com>
2024-03-01 02:41:12 +0500

    first commit

---
+++ Question 1/app.js
@@ -0,0 +1,7 @@
+var three=+prompt("Enter Number that divisible by 3");
+
+if(three % 3  ===0){
+    console.log("This Number is Correct Because It divisible by 3")
+}else{
+    console.log("Number is not divisible by 3")
+}
---
+++ Question 2/app.js
@@ -0,0 +1,7 @@
+var num = +prompt("Enter Even Or Odd Number");
+
+if(num % 2 ===0){
+    console.log("This is an Even Number ")
+}else{
+    console.log("This is an Odd Number  ")
+}
